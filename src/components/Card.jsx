fb8f4a79a02a41515156688057bbf0d13c3bc0f3
LICF01 <lucascubilla@gmail.com>
2022-01-21 09:38:14 -0300

    Added elements needed to style cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,21 @@
 // styles
 import './Card.css';
 
-const Card = ({ img, name, number }) => {
+const Card = ({ img, name, number, types }) => {
 	return (
-		<div className='card'>
-			<img src={img} alt={`${name} card`} />
-			<p className='card-name'>{name}</p>
-			<p className='card-number'>{number}</p>
+		<div
+			className='card'
+			style={{
+				backgroundColor: `var(--type-${types[0].type.name}-light)`,
+			}}
+		>
+			<div className='card-img'>
+				<img src={img} alt={`${name} card`} />
+			</div>
+			<div className='card-info'>
+				<p className='card-name'>{name}</p>
+				<p className='card-number'>{String(number).padStart(3,0)}</p>
+			</div>
 		</div>
 	);
 };
