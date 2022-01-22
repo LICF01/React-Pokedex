162f48da8a933a7b033d72e37624231a31660aa6
LICF01 <lucascubilla@gmail.com>
2022-01-22 07:30:32 -0300

    Created custom hook to fetch pokemons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import useGetPokemons from './hooks/useGetPokemons';
 
 // components
 import Card from './components/Card';
@@ -6,70 +7,33 @@ import Card from './components/Card';
 // styles
 import './App.css';
 
-const ditto = {
-	name: 'ditto',
-	id: 132,
-	img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png',
-	color: 'purple',
-};
-
 function App() {
-	const [data, setData] = useState(null);
-	const [isPending, setIsPending] = useState(false);
-	const [error, setError] = useState(null);
-	const [allPokemons, setAllPokemons] = useState([]);
-	const [loadMore, setLoadMore] = useState(
+	const { isPending, error, pokemons, hasNext } = useGetPokemons(
 		'https://pokeapi.co/api/v2/pokemon/'
 	);
-
-	const getAllPokemons = async () => {
-		setIsPending(true);
-		try {
-			const res = await fetch(loadMore);
-
-			if (!res.ok) {
-				throw new Error(res.statusText);
-			}
-
-			const data = await res.json();
-
-			for await (const pokemon of data.results) {
-				const res = await fetch(pokemon.url);
-				const data = await res.json();
-				setAllPokemons((pokemons) => [...pokemons, data]);
-				
-			}
-
-			setData(data);
-			setIsPending(false);
-			setError(null);
-		} catch (error) {
-			setError(error.message);
-			setIsPending(false);
-		}
-	};
-
-	useEffect(() => {
-		getAllPokemons();
-	}, []);
-
-	console.log(allPokemons);
 
 	return (
 		<div className='App'>
 			<h1>Pokédex</h1>
 			<p>Search for a Pokémon by name or by its Pokédex number</p>
-			<div className='card-grid'>
-				{allPokemons.map((pokemon) => (
-					<Card
-						name={pokemon.name}
-						img={pokemon.sprites.other["official-artwork"].front_default}
-						number={pokemon.id}
-						types={pokemon.types}
-						key={pokemon.id}
-					/>
-				))}
-			</div>
+			{isPending && <p>loading...</p>}
+			{error && <p>{error}</p>}
+			{pokemons && (
+				<div className='card-grid'>
+					{pokemons.map((pokemon) => (
+						<Card
+							key={pokemon.name}
+							name={pokemon.name}
+							img={
+								pokemon.sprites.other['official-artwork']
+									.front_default
+							}
+							number={pokemon.id}
+							types={pokemon.types}
+						/>
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
---
+++ src/hooks/useGetPokemons.js
@@ -0,0 +1,41 @@
+import { useEffect, useState, useCallback } from 'react';
+
+function useGetPokemons(url) {
+	const [isPending, setIsPending] = useState(false);
+	const [error, setError] = useState(null);
+	const [pokemons, setPokemons] = useState([]);
+	const [hasNext, setHasNext] = useState(null)
+
+	const getPokemons = useCallback(async () => {
+		setIsPending(true);
+		try {
+			const res = await fetch(url);
+
+			if (!res.ok) {
+				throw new Error(res.statusText);
+			}
+			const data = await res.json();
+
+			for await (const pokemon of data.results) {
+				const res = await fetch(pokemon.url);
+				const data = await res.json();
+				setPokemons((pokemons) => [...pokemons, data]);
+			}
+
+			setHasNext(data.next);
+			setIsPending(false);
+			setError(null);
+		} catch (error) {
+			setError(error.message);
+			setIsPending(false);
+		}
+	}, [url]);
+
+	useEffect(() => {
+		getPokemons();
+	}, [url, getPokemons]);
+
+	return { isPending, error, pokemons, hasNext };
+}
+
+export default useGetPokemons;
