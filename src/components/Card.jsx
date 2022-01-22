0cbe674bda2abcaeb05f6dbd738bb0ae176ee34b
LICF01 <lucascubilla@gmail.com>
2022-01-22 07:31:46 -0300

    Bug solved, forgot to add a key when maping types

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,25 @@
 // styles
 import './Card.css';
 
-const Card = ({ img, name, number, types }) => {
+const Card = ({ img, name, number, types}) => {
 	return (
 		<div
 			className='card'
 			style={{
-				backgroundColor: `var(--type-${types[0].type.name}-light)`,
+				backgroundColor: `var(--type-${types[0].type.name})`,
 			}}
 		>
 			<div className='card-img'>
 				<img src={img} alt={`${name} card`} />
 			</div>
 			<div className='card-info'>
+				<div className='pokemon-types'>
+					{types.map((type) => (
+						<p key={type.type.name}>{type.type.name}</p>
+					))}
+				</div>
 				<p className='card-name'>{name}</p>
-				<p className='card-number'>{String(number).padStart(3,0)}</p>
+				<p className='card-number'>{String(number).padStart(3, 0)}</p>
 			</div>
 		</div>
 	);
